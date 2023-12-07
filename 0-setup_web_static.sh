72510408d52c27e9ed33058df1671404172a4325
Younes_Khalloufi <younes.khaloufi.1998@gmail.com>
2023-12-07 08:12:53 -0800

    done

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,50 +1,28 @@
 #!/usr/bin/env bash
-# a Bash script that sets up your web servers for the deployment of web_static
+# Bash script that sets up your web servers for the deployment of web_static
 
-# Install Nginx if not already installed
-if ! command -v nginx &> /dev/null; then
-    sudo apt-get update
-    sudo apt-get -y install nginx
+apt-get -y update > dev/null
+apt-get install -y nginx > /dev/null
+
+# Create all necessary directories and file
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+touch /data/web_static/releases/test/index.html
+echo "Hello World again!" > /data/web_static/releases/test/index.html
+
+# Check if directory current exists
+if [ -d "/data/web_static/current" ]; then
+    sudo rm -rf /data/web_static/current
 fi
 
-# Create necessary directories if they don't exist
-sudo mkdir -p /data/web_static/{releases/test,shared}
+# Create a symbolic link to test
+ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-# Create a fake HTML file
-echo "Fake Content" | sudo tee /data/web_static/releases/test/index.html > /dev/null
+# Change ownership to user ubuntu
+chown -R ubuntu:ubuntu /data
 
-# Create or recreate the symbolic link
-sudo rm -f /data/web_static/current
-sudo ln -s /data/web_static/releases/test /data/web_static/current
+# Configure nginx to serve content pointed to by symbolic link to hbnb_static
+sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 
-# Give ownership of the /data/ folder to the ubuntu user and group recursively
-sudo chown -R ubuntu:ubuntu /data/
-
-# Update Nginx configuration
-nginx_config="/etc/nginx/sites-available/default"
-
-# Create a temporary Nginx configuration file
-cat > /tmp/nginx_config <<EOF
-server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-
-    server_name _;
-
-    location /hbnb_static/ {
-        alias /data/web_static/current/;
-    }
-
-    location / {
-        try_files \$uri \$uri/ =404;
-    }
-}
-EOF
-
-# Replace the Nginx configuration file with the temporary file
-sudo mv /tmp/nginx_config "$nginx_config"
-
-# Restart Nginx to apply changes
-sudo service nginx restart
-
-echo "Web server setup complete."
+# Restart server
+service nginx restart
